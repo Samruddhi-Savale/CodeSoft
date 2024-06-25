3ad23491407dc724f524056432bcb9324d3c13b7
Samruddhi-Savale <144936889+Samruddhi-Savale@users.noreply.github.com>
2024-06-25 16:14:13 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,53 @@
+#1
+print("Hello World\n")
+
+#2
+x = 5
+y = "Hello"
+print(x)
+print(y,"\n")
+
+#3
+a = 6
+a = "SAM"
+print(a)
+
+#4
+p = q = r = "Mango"
+print("\n")
+print(p)
+print(q)
+print(r)
+print("\n")
+
+#5
+s = "Welcome"
+print(s + " To The Python Language!")
+print('\n')
+
+#6
+b = "Python is "
+c = "awesome"
+d = b + c
+print(d)
+
+#7
+X = 5
+Y = 10
+print('\n')
+print(X + Y)
+
+#8
+A, B, C = 5, 10, 15
+print('\n')
+print(A)
+print(B)
+print(C)
+
+#9
+L = "4"
+M = "6"
+N = "2"
+O = L + M + N
+print('\n')
+print(O)
